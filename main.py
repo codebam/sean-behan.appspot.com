e13d4dfc79be5dd560e863498d1a96f60c5775af
Sean Behan <codebam@riseup.net>
2016-07-12 02:11:17 -0400

    flexible

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-import os
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
-from google.appengine.ext.webapp import template
+import logging
 
-class MainHandler(webapp.RequestHandler):
-  def get (self, q):
-    if q is None:
-      q = 'index.html'
+from flask import Flask, render_template, request, send_from_directory
+from werkzeug.routing import BaseConverter
 
-    path = os.path.join (os.path.dirname (__file__), q)
-    self.response.headers ['Content-Type'] = 'text/html'
-    self.response.out.write (template.render (path, {}))
 
-application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
+app = Flask(__name__)
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+app.url_map.converters['regex'] = RegexConverter
+
+@app.route('/')
+def render_():
+    return send_from_directory('', 'index.html')
+
+@app.route('/<regex("[\W\S\D]*"):filename>')
+def render(filename):
+    #return send_from_directory('', filename)
+    try:
+        #myfilename = str(list(request.args)[0])
+        return send_from_directory('', filename)
+    except:
+        #return str(request.args)
+        return send_from_directory('', 'index.html')
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8081, debug=True)
